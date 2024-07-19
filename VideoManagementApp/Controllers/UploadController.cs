22720af228dece4a39066c0d6d26c4870c67b95a
Mona Khimani <mona.khimani@familylawapp.com.au>
2024-07-19 12:14:17 +1000

    Initial Commit

---
+++ VideoManagementApp/Controllers/UploadController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VideoManagementApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _env;
+        public UploadController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        [HttpPost]
+        [RequestSizeLimit(200 * 1024 * 1024)]
+        public async Task<IActionResult> UploadFiles(List<IFormFile> files)
+        {           
+            if (files == null || files.Count == 0)
+            {
+                return BadRequest(new { error = "No files received." });
+            }
+
+            foreach (var file in files)
+            {              
+                if (Path.GetExtension(file.FileName).ToLower() != ".mp4")
+                {
+                    return BadRequest(new { error = "Only MP4 files are allowed." });
+                }
+
+                var filePath = Path.Combine(_env.WebRootPath, "media", file.FileName);
+                try
+                {
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await file.CopyToAsync(stream);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return StatusCode(500, new { error = $"An error occurred while saving the file: {ex.Message}" });
+                }
+            }
+            return Ok(new { message = "Files uploaded successfully." });
+        }
+    }
+}
